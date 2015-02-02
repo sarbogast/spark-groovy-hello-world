2b49b221aa10b66c412bf0440fd193a43628444c
sarbogast <sebastien@instaply.com>
2015-02-02 12:45:08 +0100

    Add freemarker support

--- src/main/groovy/WebServer.groovy
+++ src/main/groovy/WebServer.groovy
@@ -1,12 +1,19 @@
+import spark.ModelAndView
+
 import static spark.Spark.*
+import spark.template.freemarker.FreeMarkerEngine
  
 class WebServer {
  
   static void main(String[] args) {
-      port Integer.valueOf(System.getenv("PORT")) ?: 4567
+      def systemPort = System.getenv("PORT")
+      if(systemPort) {
+          port Integer.valueOf(systemPort)
+      }
+      staticFileLocation '/public'
 
       get "/hello/:name", { request, response ->
-          return "Hello " + request.params('name') + "\n"
-      }
+          return new ModelAndView([greeted: request.params('name')], "hello.ftl")
+      }, new FreeMarkerEngine()
   }
 }
---
+++ src/main/resources/spark/template/freemarker/hello.ftl
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Hello, ${greeted}!</title>
+    <link rel="stylesheet" href="css/styles.css"/>
+</head>
+<body>
+<h1>Hello ${greeted}</h1>
+<h1>The above text is set using a FreeMarkerEngine</h1>
+</body>
+</html>
