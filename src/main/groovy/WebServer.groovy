786b558c2abb65b317b5e80bfba781f8ca8bfcd7
sarbogast <sebastien@instaply.com>
2015-02-02 11:07:05 +0100

    Change port

--- src/main/groovy/WebServer.groovy
+++ src/main/groovy/WebServer.groovy
@@ -3,6 +3,8 @@ import static spark.Spark.*
 class WebServer {
  
   static void main(String[] args) {
+      port Integer.valueOf(System.getenv("PORT")) ?: 4567
+
       get "/hello/:name", { request, response ->
           return "Hello " + request.params('name') + "\n"
       }
